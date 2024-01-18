c5e6f6f9c2d7784f815c65257b79eb4236720f0c
Michael Bucher <michael@axlabs.com>
2024-01-18 17:17:33 +0100

    (#32) Improve test helper util methods

--- src/test/java/network/bane/util/SmartContractHelper.java
+++ src/test/java/network/bane/util/SmartContractHelper.java
@@ -4,12 +4,16 @@ import io.neow3j.contract.SmartContract;
 import io.neow3j.crypto.Base64;
 import io.neow3j.protocol.Neow3j;
 import io.neow3j.protocol.core.response.ContractStorageEntry;
+import io.neow3j.protocol.core.response.NeoSendRawTransaction;
+import io.neow3j.transaction.TransactionBuilder;
 import io.neow3j.types.Hash160;
+import io.neow3j.types.Hash256;
 
 import java.io.IOException;
 import java.math.BigInteger;
 import java.util.List;
 
+import static io.neow3j.utils.Await.waitUntilTransactionIsExecuted;
 import static io.neow3j.utils.Numeric.cleanHexPrefix;
 import static io.neow3j.utils.Numeric.toHexString;
 
@@ -17,6 +21,13 @@ public class SmartContractHelper extends SmartContract {
 
     public SmartContractHelper(Hash160 scriptHash, Neow3j neow3j) {
         super(scriptHash, neow3j);
+    }
+
+    public Hash256 sendAndAwaitExecution(TransactionBuilder b) throws Throwable {
+        NeoSendRawTransaction response = b.sign().send();
+        Hash256 txHash = response.getSendRawTransaction().getHash();
+        waitUntilTransactionIsExecuted(txHash, neow3j);
+        return txHash;
     }
 
     public List<ContractStorageEntry> findStorage(String prefixHex) throws IOException {
