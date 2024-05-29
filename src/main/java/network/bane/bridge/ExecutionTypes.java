c34cf24abfe400218116f5b4d3faef9e30de4b50
Michael Bucher <michael@axlabs.com>
2024-05-29 11:31:09 +0200

    Rename TokenType to ExecutionType

---
+++ src/main/java/network/bane/bridge/ExecutionTypes.java
@@ -0,0 +1,10 @@
+package network.bane.bridge;
+
+public class ExecutionTypes {
+
+    static final int NEO = 0;
+    static final int NEP17 = 1;
+
+    public static final int MAX_EXECUTION_TYPE_VALUE = 1;
+
+}
