841858b38849061113ae58e896b0ab6bb2320959
Michael Bucher <michael@axlabs.com>
2024-06-07 19:09:06 +0200

    (#48) Add better update logic and allow data in update function

--- src/main/java/network/bane/bridge/BridgeContract.java
+++ src/main/java/network/bane/bridge/BridgeContract.java
@@ -16,7 +16,6 @@ import io.neow3j.devpack.annotations.OnDeployment;
 import io.neow3j.devpack.annotations.OnNEP17Payment;
 import io.neow3j.devpack.annotations.Permission;
 import io.neow3j.devpack.annotations.Safe;
-import io.neow3j.devpack.constants.NativeContract;
 import io.neow3j.devpack.contracts.ContractManagement;
 import io.neow3j.devpack.contracts.CryptoLib;
 import io.neow3j.devpack.contracts.GasToken;
@@ -175,7 +174,7 @@ public class BridgeContract {
     static Event4Args<Hash160, Integer, Hash160, Integer> onTokenWithdrawal;
 
     @DisplayName("TokenWithdrawalRootUpdate")
-    @EventParameterNames({"NeoN3Token", "NeoXToken","Nonce", "WithdrawalRoot"})
+    @EventParameterNames({"NeoN3Token", "NeoXToken", "Nonce", "WithdrawalRoot"})
     static Event4Args<Hash160, Hash160, Integer, ByteString> onTokenWithdrawalRootUpdate;
 
     @DisplayName("TokenDepositFeeChange")
@@ -198,74 +197,35 @@ public class BridgeContract {
     // endregion
     // region deployment/update
 
-    /**
-     * This method is only used for the T3 contract update migration.
-     * Once that contract update is done, this method will be removed from this contract.
-     */
-    public static void migrate(int gasMaxWithdrawals) {
-        if (!baseMap.getBoolean(OLD_KEY_PAUSED)) abort("Contract must be paused for migration.");
-        if (gasMaxWithdrawals <= 0) abort("Max withdrawals must be positive.");
-        if (baseMap.get(KEY_MIGRATED) != null) abort("Already migrated.");
-        baseMap.put(KEY_MIGRATED, 1);
-
-        Integer gasDepositFee = baseMap.getInt(OLD_KEY_GAS_DEPOSIT_FEE);
-        baseMap.delete(OLD_KEY_GAS_DEPOSIT_FEE);
-        Integer gasMinDepositAmount = baseMap.getInt(OLD_KEY_GAS_DEPOSIT_MIN_AMOUNT);
-        baseMap.delete(OLD_KEY_GAS_DEPOSIT_MIN_AMOUNT);
-        Integer gasMaxDepositAmount = baseMap.getInt(OLD_KEY_GAS_DEPOSIT_MAX_AMOUNT);
-        baseMap.delete(OLD_KEY_GAS_DEPOSIT_MAX_AMOUNT);
-        Integer pausedState = baseMap.getInt(OLD_KEY_PAUSED);
-        baseMap.delete(OLD_KEY_PAUSED);
-
-        ByteString gasDepositRoot = baseMap.get(OLD_KEY_GAS_DEPOSIT_ROOT);
-        baseMap.delete(OLD_KEY_GAS_DEPOSIT_ROOT);
-        int gasDepositNonce = baseMap.getInt(OLD_KEY_GAS_DEPOSIT_NONCE);
-        baseMap.delete(OLD_KEY_GAS_DEPOSIT_NONCE);
-
-        ByteString gasWithdrawalRoot = baseMap.get(OLD_KEY_GAS_WITHDRAWAL_ROOT);
-        baseMap.delete(OLD_KEY_GAS_WITHDRAWAL_ROOT);
-        int gasWithdrawalNonce = baseMap.getInt(OLD_KEY_GAS_WITHDRAWAL_NONCE);
-        baseMap.delete(OLD_KEY_GAS_WITHDRAWAL_NONCE);
-        // At this stage every storage entry that needs to be migrated has been read and deleted.
-
-        // Now we can write the new storage entries.
-        baseMap.put(KEY_BRIDGE_PAUSE, pausedState);
-        GasConfig gasConfig = new GasConfig(gasDepositFee, gasMinDepositAmount, gasMaxDepositAmount, gasMaxWithdrawals);
-
-        State depositState = new State(gasDepositNonce, gasDepositRoot);
-        State withdrawalState = new State(gasWithdrawalNonce, gasWithdrawalRoot);
-        GasBridge gasBridge = new GasBridge(false, depositState, withdrawalState, gasConfig);
-        assert GasBridge.isValid(gasBridge) : "Invalid gas bridge state.";
-        ByteString serialized = new StdLib().serialize(gasBridge);
-        baseMap.put(KEY_GAS_BRIDGE, serialized);
-        // In order to remain simple here as this is only used for the T3 contract update, the already collected fees
-        // up to this migration are ignored.
-        baseMap.put(KEY_UNCLAIMED_REWARDS, 0);
-    }
-
     @OnDeployment
     public static void deploy(Object data, boolean isUpdate) {
-        if (!isUpdate) {
-            BridgeDeploymentData deploymentData = (BridgeDeploymentData) data;
-            if (deploymentData.bridgeManagementContract == null ||
-                    !Hash160.isValid(deploymentData.bridgeManagementContract))
-                abort("Invalid bridge management contract hash.");
-
-            baseMap.put(KEY_BRIDGE_MANAGEMENT, deploymentData.bridgeManagementContract);
-            baseMap.put(KEY_BRIDGE_PAUSE, false);
-
-            if (!GasConfig.isValid(deploymentData.gasConfig)) abort("Invalid gas config.");
-            ByteString zeroHash = Hash256.zero().toByteString();
-            State newDepositState = new State(0, zeroHash);
-            State newWithdrawalState = new State(0, zeroHash);
-            GasBridge gasBridge = new GasBridge(false, newDepositState, newWithdrawalState, deploymentData.gasConfig);
-            ByteString serialize = new StdLib().serialize(gasBridge);
-            baseMap.put(KEY_GAS_BRIDGE, serialize);
-            baseMap.put(KEY_UNCLAIMED_REWARDS, 0);
-
-            if (!checkWitness(managementContract().owner())) {
-                abort("Owner must witness the deployment.");
+        if (isUpdate) {
+            if (baseMap.get(KEY_MIGRATED) == null) {
+                MigrationImpl_V1ToV2.migrateT3_v1Tov2();
             }
+            return;
+        }
+
+        BridgeDeploymentData deploymentData = (BridgeDeploymentData) data;
+        if (deploymentData.bridgeManagementContract == null ||
+                !Hash160.isValid(deploymentData.bridgeManagementContract))
+            abort("Invalid bridge management contract hash.");
+
+        baseMap.put(KEY_BRIDGE_MANAGEMENT, deploymentData.bridgeManagementContract);
+        baseMap.put(KEY_BRIDGE_PAUSE, false);
+
+        if (!GasConfig.isValid(deploymentData.gasConfig)) abort("Invalid gas config.");
+        ByteString zeroHash = Hash256.zero().toByteString();
+        State newDepositState = new State(0, zeroHash);
+        State newWithdrawalState = new State(0, zeroHash);
+        GasBridge gasBridge = new GasBridge(false, newDepositState, newWithdrawalState, deploymentData.gasConfig);
+        ByteString serialize = new StdLib().serialize(gasBridge);
+        baseMap.put(KEY_GAS_BRIDGE, serialize);
+        baseMap.put(KEY_UNCLAIMED_REWARDS, 0);
+
+        // Make sure the owner witnesses the deployment.
+        if (!checkWitness(managementContract().owner())) {
+            abort("Owner must witness the deployment.");
         }
     }
 
---
+++ src/main/java/network/bane/bridge/MigrationImpl_V1ToV2.java
@@ -0,0 +1,71 @@
+package network.bane.bridge;
+
+import io.neow3j.devpack.ByteString;
+import io.neow3j.devpack.contracts.StdLib;
+import network.bane.structs.GasBridge;
+import network.bane.structs.GasConfig;
+import network.bane.structs.State;
+
+
+import static io.neow3j.devpack.Helper.abort;
+import static network.bane.bridge.BridgeContract.baseMap;
+import static network.bane.bridge.StorageConstants.KEY_BRIDGE_PAUSE;
+import static network.bane.bridge.StorageConstants.KEY_GAS_BRIDGE;
+import static network.bane.bridge.StorageConstants.KEY_MIGRATED;
+import static network.bane.bridge.StorageConstants.KEY_UNCLAIMED_REWARDS;
+import static network.bane.bridge.StorageConstants.OLD_KEY_GAS_DEPOSIT_FEE;
+import static network.bane.bridge.StorageConstants.OLD_KEY_GAS_DEPOSIT_MAX_AMOUNT;
+import static network.bane.bridge.StorageConstants.OLD_KEY_GAS_DEPOSIT_MIN_AMOUNT;
+import static network.bane.bridge.StorageConstants.OLD_KEY_GAS_DEPOSIT_NONCE;
+import static network.bane.bridge.StorageConstants.OLD_KEY_GAS_DEPOSIT_ROOT;
+import static network.bane.bridge.StorageConstants.OLD_KEY_GAS_WITHDRAWAL_NONCE;
+import static network.bane.bridge.StorageConstants.OLD_KEY_GAS_WITHDRAWAL_ROOT;
+import static network.bane.bridge.StorageConstants.OLD_KEY_PAUSED;
+
+public class MigrationImpl_V1ToV2 {
+
+    /**
+     * This method is only used for the T3 contract update migration.
+     * Once that contract update is done, this method will be removed from this contract.
+     */
+    static void migrateT3_v1Tov2() {
+        if (!baseMap.getBoolean(OLD_KEY_PAUSED)) abort("Contract must be paused for migration.");
+        if (baseMap.get(KEY_MIGRATED) != null) abort("Already migrated.");
+        baseMap.put(KEY_MIGRATED, 1);
+
+        Integer gasDepositFee = baseMap.getInt(OLD_KEY_GAS_DEPOSIT_FEE);
+        baseMap.delete(OLD_KEY_GAS_DEPOSIT_FEE);
+        Integer gasMinDepositAmount = baseMap.getInt(OLD_KEY_GAS_DEPOSIT_MIN_AMOUNT);
+        baseMap.delete(OLD_KEY_GAS_DEPOSIT_MIN_AMOUNT);
+        Integer gasMaxDepositAmount = baseMap.getInt(OLD_KEY_GAS_DEPOSIT_MAX_AMOUNT);
+        baseMap.delete(OLD_KEY_GAS_DEPOSIT_MAX_AMOUNT);
+        Integer pausedState = baseMap.getInt(OLD_KEY_PAUSED);
+        baseMap.delete(OLD_KEY_PAUSED);
+
+        ByteString gasDepositRoot = baseMap.get(OLD_KEY_GAS_DEPOSIT_ROOT);
+        baseMap.delete(OLD_KEY_GAS_DEPOSIT_ROOT);
+        int gasDepositNonce = baseMap.getInt(OLD_KEY_GAS_DEPOSIT_NONCE);
+        baseMap.delete(OLD_KEY_GAS_DEPOSIT_NONCE);
+
+        ByteString gasWithdrawalRoot = baseMap.get(OLD_KEY_GAS_WITHDRAWAL_ROOT);
+        baseMap.delete(OLD_KEY_GAS_WITHDRAWAL_ROOT);
+        int gasWithdrawalNonce = baseMap.getInt(OLD_KEY_GAS_WITHDRAWAL_NONCE);
+        baseMap.delete(OLD_KEY_GAS_WITHDRAWAL_NONCE);
+        // At this stage every storage entry that needs to be migrated has been read and deleted.
+
+        // Now we can write the new storage entries.
+        baseMap.put(KEY_BRIDGE_PAUSE, pausedState);
+        GasConfig gasConfig = new GasConfig(gasDepositFee, gasMinDepositAmount, gasMaxDepositAmount, 100);
+
+        State depositState = new State(gasDepositNonce, gasDepositRoot);
+        State withdrawalState = new State(gasWithdrawalNonce, gasWithdrawalRoot);
+        GasBridge gasBridge = new GasBridge(false, depositState, withdrawalState, gasConfig);
+        assert GasBridge.isValid(gasBridge) : "Invalid gas bridge state.";
+        ByteString serialized = new StdLib().serialize(gasBridge);
+        baseMap.put(KEY_GAS_BRIDGE, serialized);
+        // In order to remain simple here as this is only used for the T3 contract update, the already collected fees
+        // up to this migration are ignored.
+        baseMap.put(KEY_UNCLAIMED_REWARDS, 0);
+    }
+
+}
