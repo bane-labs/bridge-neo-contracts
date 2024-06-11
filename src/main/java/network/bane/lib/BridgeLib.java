341e7d98486e7d413c2afffa8d4ead8b25266ea7
Michael Bucher <michael@axlabs.com>
2024-06-11 08:11:48 +0200

    (minor) Reduce visibility of constant only used in lib package

--- src/main/java/network/bane/lib/BridgeLib.java
+++ src/main/java/network/bane/lib/BridgeLib.java
@@ -9,7 +9,7 @@ import static io.neow3j.devpack.Helper.concat;
 
 public class BridgeLib {
 
-    public static final int UINT256_SIZE = 32;
+    static final int UINT256_SIZE = 32;
 
     public static ByteString computeNewRoot(CryptoLib cryptoLib, ByteString left, ByteString right) {
         ByteString leftRight = new ByteString(concat(left.toByteArray(), right));
