5e5c58fbb1d57905a6f054205fa43fa80ab3947d
Michael Bucher <michael@axlabs.com>
2024-07-17 21:18:02 +0200

    (#114) Use Hash160 for non-validator roles

--- src/main/java/network/bane/structs/ManagementDeploymentData.java
+++ src/main/java/network/bane/structs/ManagementDeploymentData.java
@@ -1,17 +1,18 @@
 package network.bane.structs;
 
 import io.neow3j.devpack.ECPoint;
+import io.neow3j.devpack.Hash160;
 import io.neow3j.devpack.List;
 import io.neow3j.devpack.annotations.Struct;
 
 @Struct
 public class ManagementDeploymentData {
 
-    public ECPoint owner;
-    public ECPoint relayer;
+    public Hash160 owner;
+    public Hash160 relayer;
     public List<ECPoint> validators;
     public int validatorThreshold;
-    public ECPoint governor;
-    public ECPoint securityGuard;
+    public Hash160 governor;
+    public Hash160 securityGuard;
 
 }
