c2b1ccb37da127e5600232478d05fd081c300259
Michael Bucher <michael@axlabs.com>
2024-07-24 07:43:22 +0200

    (minor) Fix outdated import

--- src/main/java/network/bane/bridge/MigrationT4V1ToV2.java
+++ src/main/java/network/bane/bridge/MigrationT4V1ToV2.java
@@ -10,7 +10,7 @@ import network.bane.structs.GasConfigV1;
 
 import static io.neow3j.devpack.Helper.abort;
 import static network.bane.bridge.StorageConstants.KEY_GAS_BRIDGE;
-import static network.bane.bridge.StorageConstants.KEY_MIGRATED;
+import static network.bane.bridge.StorageConstants.KEY_VERSION;
 
 public class MigrationT4V1ToV2 {
 
@@ -35,7 +35,7 @@ public class MigrationT4V1ToV2 {
             abort("Invalid gas bridge state.");
         }
         BridgeContract.baseMap.put(KEY_GAS_BRIDGE, new StdLib().serialize(newGasBridge));
-        BridgeContract.baseMap.put(KEY_MIGRATED, 0);
+        BridgeContract.baseMap.put(KEY_VERSION, 0);
     }
 
 }
