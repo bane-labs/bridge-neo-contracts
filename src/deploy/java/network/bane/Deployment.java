62a6e60021acf03e157827625c6f1ffb0a4cfee4
Michael Bucher <michael@axlabs.com>
2024-10-02 19:10:43 +0200

    Update deployment script

--- src/deploy/java/network/bane/Deployment.java
+++ src/deploy/java/network/bane/Deployment.java
@@ -3,10 +3,12 @@ package network.bane;
 import io.neow3j.compiler.CompilationUnit;
 import io.neow3j.compiler.Compiler;
 import io.neow3j.contract.ContractManagement;
+import io.neow3j.contract.NefFile;
 import io.neow3j.contract.SmartContract;
 import io.neow3j.crypto.ECKeyPair;
 import io.neow3j.crypto.ECKeyPair.ECPublicKey;
 import io.neow3j.protocol.Neow3j;
+import io.neow3j.protocol.core.response.ContractManifest;
 import io.neow3j.protocol.core.response.ContractState;
 import io.neow3j.protocol.core.response.NeoApplicationLog;
 import io.neow3j.protocol.core.response.NeoSendRawTransaction;
@@ -23,14 +25,18 @@ import network.bane.bridge.BridgeContract;
 import network.bane.management.BridgeManagementContract;
 
 import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import static io.neow3j.types.ContractParameter.array;
 import static io.neow3j.types.ContractParameter.hash160;
 import static io.neow3j.types.ContractParameter.integer;
-import static io.neow3j.types.ContractParameter.publicKey;
 import static java.lang.String.format;
 import static java.util.Arrays.asList;
+import static network.bane.utils.GetEnv.getEnvVariableOrDefault;
+import static network.bane.utils.LoadWallet.getDeployerAccountFromWallet;
+import static network.bane.utils.LoadWallet.getOwnerAccountFromWallet;
 
 /**
  * This class is used to create the deployment transactions for the Bridge Management and Bridge in a private net.
@@ -39,48 +45,62 @@ public class Deployment {
 
     private static final String NODE = getEnvVariableOrDefault("NEON3_JSON_RPC", "http://127.0.0.1:40332");
 
-    private static final Account deployerAcc = Account.fromWIF(getEnvVariableOrDefault("NEON3_DEPLOYER_WIF", ""));
-    private static final Hash160 deployer = deployerAcc.getScriptHash();
+    private static final String managementContractName =
+            getEnvVariableOrDefault("MANAGEMENT_CONTRACT_NAME", "BridgeManagement");
+    private static final String bridgeContractName =
+            getEnvVariableOrDefault("BRIDGE_CONTRACT_NAME", "Bridge");
 
-    private static final Account ownerAcc = Account.fromWIF(getEnvVariableOrDefault("NEON3_OWNER_WIF", ""));
-    private static final Hash160 owner = ownerAcc.getScriptHash();
+    private static Account deployerAcc;
+    private static Account ownerAcc;
 
-    private static final Hash160 relayer = new Hash160(
-            getEnvVariableOrDefault("NEON3_RELAYER", "0x77ddabed5b98bef73b1592cb588118d2b8ab4a4c"));
+    private static final Hash160 relayer = Hash160.fromAddress(
+            getEnvVariableOrDefault("NEON3_RELAYER_ADDRESS", "NSsMzYzdLsXG6mjp4MHyGZHuKssakqpv3g"));
 
+    private static final int MAX_NR_VALIDATORS = 7;
+    // This env variable specifies the number of validators (n). The first n validators are used and remaining
+    // validators will be ignored. If n is lower than 2 or higher than 7, an exception will be thrown.
+    private static final int nrValidators = Integer.parseInt(getEnvVariableOrDefault("NEON3_NR_VALIDATORS", "7"));
     private static final ECPublicKey validator_1 = new ECPublicKey(
-            getEnvVariableOrDefault("NEON3_VALIDATOR1_PUBKEY", "021fed0d208f2c4b2fe425571c36aea1d465159c93af349581896fe38553dffae1"));
+            getEnvVariableOrDefault("NEON3_VALIDATOR1_PUBKEY",
+                    "021fed0d208f2c4b2fe425571c36aea1d465159c93af349581896fe38553dffae1"));
     private static final ECPublicKey validator_2 = new ECPublicKey(
-            getEnvVariableOrDefault("NEON3_VALIDATOR2_PUBKEY", "021986f90b2596322c9f681c42814237555732411e8c517100c702b91cd404f090"));
+            getEnvVariableOrDefault("NEON3_VALIDATOR2_PUBKEY",
+                    "021986f90b2596322c9f681c42814237555732411e8c517100c702b91cd404f090"));
     private static final ECPublicKey validator_3 = new ECPublicKey(
-            getEnvVariableOrDefault("NEON3_VALIDATOR3_PUBKEY", "021387fa5748344658778d9a3de2bd99f2133b96a3fe5ab2cda007bce3531f4d9f"));
+            getEnvVariableOrDefault("NEON3_VALIDATOR3_PUBKEY",
+                    "021387fa5748344658778d9a3de2bd99f2133b96a3fe5ab2cda007bce3531f4d9f"));
     private static final ECPublicKey validator_4 = new ECPublicKey(
-            getEnvVariableOrDefault("NEON3_VALIDATOR4_PUBKEY", "02108e50c32a9b4c12b90013b145370530a413a605ced93491ba2523d903295f45"));
+            getEnvVariableOrDefault("NEON3_VALIDATOR4_PUBKEY",
+                    "02108e50c32a9b4c12b90013b145370530a413a605ced93491ba2523d903295f45"));
     private static final ECPublicKey validator_5 = new ECPublicKey(
-            getEnvVariableOrDefault("NEON3_VALIDATOR5_PUBKEY", "030187e4b19cddfa93f282c0bba9759446094c006a9f91c5e80963b2d9f8c4b568"));
+            getEnvVariableOrDefault("NEON3_VALIDATOR5_PUBKEY",
+                    "030187e4b19cddfa93f282c0bba9759446094c006a9f91c5e80963b2d9f8c4b568"));
     private static final ECPublicKey validator_6 = new ECPublicKey(
-            getEnvVariableOrDefault("NEON3_VALIDATOR6_PUBKEY", "037c94e4ef2445f283ab7b02a1c26783ab39527404c157f4eb686edbdd0e651ade"));
+            getEnvVariableOrDefault("NEON3_VALIDATOR6_PUBKEY",
+                    "037c94e4ef2445f283ab7b02a1c26783ab39527404c157f4eb686edbdd0e651ade"));
     private static final ECPublicKey validator_7 = new ECPublicKey(
-            getEnvVariableOrDefault("NEON3_VALIDATOR7_PUBKEY", "02189efe8f9d4fc34cceaae9fa79346525810143d5f198070594c7da12facfd4ab"));
+            getEnvVariableOrDefault("NEON3_VALIDATOR7_PUBKEY",
+                    "02189efe8f9d4fc34cceaae9fa79346525810143d5f198070594c7da12facfd4ab"));
 
-    private static final int validator_threshold = Integer.parseInt(getEnvVariableOrDefault("NEON3_VALIDATOR_THRESHOLD", "5"));
+    private static final int validator_threshold =
+            Integer.parseInt(getEnvVariableOrDefault("NEON3_VALIDATOR_THRESHOLD", "5"));
 
-    private static final Hash160 governor = new Hash160(
-            getEnvVariableOrDefault("NEON3_GOVERNOR", "0x6d992a5e281480ba61dee66d3d008d774fa761ac"));
+    private static final Hash160 governor = Hash160.fromAddress(
+            getEnvVariableOrDefault("NEON3_GOVERNOR_ADDRESS", "NbdSPqc4iADXLT6XzFUtwhDKAfPU8JdT5b"));
 
-    private static final Hash160 securityGuard = new Hash160(
-            getEnvVariableOrDefault("NEON3_SECURITYGUARD", "0x84bd782e66c5fde259f000f7208de674f8ceaf00"));
+    private static final Hash160 securityGuard = Hash160.fromAddress(
+            getEnvVariableOrDefault("NEON3_SECURITYGUARD_ADDRESS", "NKybo9Fy5d84RFeWGMvqJgKtEwDsD1CrDD"));
 
     // Parameters
 
-    private static final BigInteger depositFee = new BigInteger(getEnvVariableOrDefault("NEON3_DEPOSIT_FEE", "10000000"));
-    private static final BigInteger minDepositAmount = new BigInteger(getEnvVariableOrDefault("NEON3_MIN_DEPOSIT_AMOUNT", "100000000"));
-    private static final BigInteger maxDepositAmount = new BigInteger(getEnvVariableOrDefault("NEON3_MAX_DEPOSIT_AMOUNT", "1000000000000"));
-
-    public static String getEnvVariableOrDefault(String variableName, String defaultValue) {
-        String value = System.getenv(variableName);
-        return value != null ? value : defaultValue;
-    }
+    private static final BigInteger depositFee =
+            new BigInteger(getEnvVariableOrDefault("NEON3_DEPOSIT_FEE", "10000000"));
+    private static final BigInteger minDepositAmount =
+            new BigInteger(getEnvVariableOrDefault("NEON3_MIN_DEPOSIT_AMOUNT", "100000000"));
+    private static final BigInteger maxDepositAmount =
+            new BigInteger(getEnvVariableOrDefault("NEON3_MAX_DEPOSIT_AMOUNT", "1000000000000"));
+    private static final BigInteger maxTotalDeposited =
+            new BigInteger(getEnvVariableOrDefault("NEON3_MAX_TOTAL_DEPOSITED", "100000000000000"));
 
     public static ContractParameter prepareManagementDeployParameter(
             Hash160 owner,
@@ -93,15 +113,7 @@ public class Deployment {
         return array(
                 hash160(owner),
                 hash160(relayer),
-                array(
-                        publicKey(validators.get(0)),
-                        publicKey(validators.get(1)),
-                        publicKey(validators.get(2)),
-                        publicKey(validators.get(3)),
-                        publicKey(validators.get(4)),
-                        publicKey(validators.get(5)),
-                        publicKey(validators.get(6))
-                ),
+                array(validators),
                 integer(threshold),
                 hash160(governor),
                 hash160(securityGuard)
@@ -109,14 +121,15 @@ public class Deployment {
     }
 
     private static ContractParameter prepareBridgeDeployParameter(Hash160 managementContractHash,
-            BigInteger depositFee, BigInteger minDeposit, BigInteger maxDeposit) {
+            BigInteger depositFee, BigInteger minDeposit, BigInteger maxDeposit, BigInteger maxTotalDeposited) {
         return array(
                 hash160(managementContractHash),
                 array(
                         integer(depositFee),
                         integer(minDeposit),
                         integer(maxDeposit),
-                        integer(100)
+                        integer(100),
+                        integer(maxTotalDeposited)
                 )
         );
     }
@@ -124,33 +137,57 @@ public class Deployment {
     public static void main(String[] args) throws Throwable {
         Neow3j neow3j = Neow3j.build(new HttpService(NODE, true));
 
+        deployerAcc = getDeployerAccountFromWallet();
+        ownerAcc = getOwnerAccountFromWallet();
+
         Hash160 managementHash = compileAndPrintManagementDeploymentTxData(neow3j);
         compileAndPrintBridgeDeploymentTxData(neow3j, managementHash);
     }
 
     private static Hash160 compileAndPrintManagementDeploymentTxData(Neow3j neow3j) throws Throwable {
         // Compile the Bridge Management contract
-        CompilationUnit managementCompUnit = new Compiler().compile(BridgeManagementContract.class.getCanonicalName());
+        HashMap<String, String> substitutions = new HashMap<>();
+        substitutions.put("ContractName", managementContractName);
+        CompilationUnit managementCompUnit = new Compiler().compile(
+                BridgeManagementContract.class.getCanonicalName(),
+                substitutions
+        );
+
+        if (MAX_NR_VALIDATORS > 7) {
+            throw new Exception("Maximum number of validators cannot be greater than 7");
+        }
+        if (validator_threshold > nrValidators) {
+            throw new Exception("Validator threshold cannot be greater than the number of validators");
+        }
+        if (validator_threshold < 2) {
+            throw new Exception("Validator threshold cannot be less than 2");
+        }
+        List<ECPublicKey> completeValidatorList = asList(
+                validator_1,
+                validator_2,
+                validator_3,
+                validator_4,
+                validator_5,
+                validator_6,
+                validator_7
+        );
+        List<ECPublicKey> validatorList = new ArrayList<>();
+        // Only use the first n validators
+        for (int i = 0; i < nrValidators; i++) {
+            validatorList.add(completeValidatorList.get(i));
+        }
 
         // Prepare the deployment parameter for the bridge management contract
         ContractParameter managementDeployParameter = prepareManagementDeployParameter(
-                owner,
+                ownerAcc.getScriptHash(),
                 relayer,
-                asList(
-                        validator_1,
-                        validator_2,
-                        validator_3,
-                        validator_4,
-                        validator_5,
-                        validator_6,
-                        validator_7
-                ),
+                validatorList,
                 validator_threshold,
                 governor,
                 securityGuard
         );
 
-        Hash160 managementContractHash = SmartContract.calcContractHash(deployer,
+        Hash160 managementContractHash = SmartContract.calcContractHash(deployerAcc.getScriptHash(),
                 managementCompUnit.getNefFile().getCheckSumAsInteger(),
                 managementCompUnit.getManifest().getName());
 
@@ -189,29 +226,38 @@ public class Deployment {
             throw new Exception("Management contract hash mismatch");
         }
 
-        System.out.println("Management contract deployed successfully in transaction: " + managementDeploymentTxHash);
+        System.out.printf("\nManagement contract (%s) deployed successfully in transaction: %s\n",
+                managementContractName, managementDeploymentTxHash);
         System.out.println("Management contract hash: " + managementContractHash);
         return managementContractHash;
     }
 
     private static void compileAndPrintBridgeDeploymentTxData(Neow3j neow3j, Hash160 managementContractHash) throws Throwable {
-        // Compile the Bridge Management contract
-        CompilationUnit bridgeCompUnit = new Compiler().compile(BridgeContract.class.getCanonicalName());
+        // Compile the Bridge contract
+        HashMap<String, String> substitutions = new HashMap<>();
+        substitutions.put("ContractName", bridgeContractName);
+        CompilationUnit bridgeCompUnit = new Compiler().compile(
+                BridgeContract.class.getCanonicalName(),
+                substitutions
+        );
 
         // Prepare the deployment parameter for the bridge contract
         ContractParameter bridgeDeploymentParameter = prepareBridgeDeployParameter(
                 managementContractHash,
                 depositFee,
                 minDepositAmount,
-                maxDepositAmount
+                maxDepositAmount,
+                maxTotalDeposited
         );
 
-        Hash160 bridgeContractHash = SmartContract.calcContractHash(deployer,
-                bridgeCompUnit.getNefFile().getCheckSumAsInteger(),
-                bridgeCompUnit.getManifest().getName());
+        NefFile nefFile = bridgeCompUnit.getNefFile();
+        ContractManifest bridgeManifest = bridgeCompUnit.getManifest();
+        Hash160 bridgeContractHash = SmartContract.calcContractHash(deployerAcc.getScriptHash(),
+                nefFile.getCheckSumAsInteger(),
+                bridgeManifest.getName());
         // Build the deployment transaction
         Transaction bridgeDeploymentTx = new ContractManagement(neow3j)
-                .deploy(bridgeCompUnit.getNefFile(), bridgeCompUnit.getManifest(), bridgeDeploymentParameter)
+                .deploy(nefFile, bridgeManifest, bridgeDeploymentParameter)
                 .signers(
                         AccountSigner.none(deployerAcc),
                         AccountSigner.none(ownerAcc).setAllowedContracts(bridgeContractHash)
@@ -234,14 +280,15 @@ public class Deployment {
 
         // Get the contract hash from the deployment transaction
         ContractState bridgeContractState = neow3j.getContractState(bridgeContractHash).send().getContractState();
-        if (!bridgeContractState.getNef().getChecksum().equals(bridgeCompUnit.getNefFile().getCheckSumAsInteger())) {
+        if (!bridgeContractState.getNef().getChecksum().equals(nefFile.getCheckSumAsInteger())) {
             throw new Exception("Bridge contract NEF checksum mismatch");
         }
         if (!bridgeContractHash.equals(bridgeContractState.getHash())) {
             throw new Exception("Bridge contract hash mismatch");
         }
 
-        System.out.println("Bridge contract deployed successfully in transaction: " + bridgeDeploymentTxHash);
+        System.out.printf("Bridge contract (%s) deployed successfully in transaction: %s\n", bridgeContractName,
+                bridgeDeploymentTxHash);
         System.out.println("Bridge Contract Script Hash: " + bridgeContractHash);
     }
 
