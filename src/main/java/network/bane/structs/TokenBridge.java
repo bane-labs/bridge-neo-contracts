8e8f740bd64e8b07f2cc9cf22a63fbaaa238db7d
Michael Bucher <michael@axlabs.com>
2024-10-11 12:51:03 +0200

    Add javadoc to decimal scaling factor in TokenConfig

--- src/main/java/network/bane/structs/TokenBridge.java
+++ src/main/java/network/bane/structs/TokenBridge.java
@@ -26,11 +26,46 @@ public class TokenBridge {
 
     @Struct
     public static class TokenConfig {
+
+        /**
+         * The hash of the corresponding token on Neo X.
+         */
         public Hash160 neoXToken;
+
+        /**
+         * The fee (in GAS) that is charged for a deposit.
+         */
         public int fee;
+
+        /**
+         * The minimum amount of tokens that can be deposited in one transaction.
+         */
         public int minAmount;
+
+        /**
+         * The maximum amount of tokens that can be deposited in one transaction.
+         */
         public int maxAmount;
+
+        /**
+         * The maximum number of withdrawals that can be made in one transaction by the relayer. Additionally, this
+         * value in combination with blocks is used as a consensus for validators to know which root they need to sign.
+         */
         public int maxWithdrawals;
+
+        /**
+         * The decimal scaling factor is used to mitigate the difference in decimals between the tokens on the
+         * originating and the destination chain. Since the bridge amount is hashed and the bridge on the destination
+         * chain needs to verify the amount with the hash, the amount used for hashing must be the same on both
+         * chains. Therefore, this bridge uses the lower number of decimals as the common denominator. Hence, the
+         * following rules apply:
+         * <ul>
+         *     <li> if the decimals of the tokens are the same on both chains, the factors on both chains should
+         *     be 0. </li>
+         *     <li> if the decimals are different, the difference in decimals (positive) should be the decimal
+         *     scaling factor on the chain where the token has more decimals. </li>
+         * </ul>
+         */
         public int decimalScalingFactor;
 
         public TokenConfig(Hash160 neoXToken, int fee, int minAmount, int maxAmount, int maxWithdrawals,
