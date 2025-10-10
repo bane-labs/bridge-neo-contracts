4aea02b1e41d4b69645515e380ce9801515ba060
Michael Bucher <michael@axlabs.com>
2025-10-10 11:26:43 +0200

    chore (minor): add deployment summary in msg bridge deploy script

--- src/deploy/java/network/bane/scripts/DeployMessageBridgeContracts.java
+++ src/deploy/java/network/bane/scripts/DeployMessageBridgeContracts.java
@@ -48,6 +48,14 @@ public class DeployMessageBridgeContracts {
         Hash256 execManagerDeployTx = deployContract(neow3j, execManagerCompUnit, deploymentAccount,
                 execManagerDeployParam);
         ensureConsistentState(neow3j, executionManagerHash, execManagerCompUnit, execManagerDeployTx);
+
+        System.out.println("\n✅ All contracts deployed successfully!");
+        // This summary is for capturing the deployed contract hashes easily from the console.
+        System.out.println("-----------------------------------");
+        System.out.println("BridgeManagement contract hash: " + managementContract);
+        System.out.println("MessageBridge contract hash:    " + messageBridgeHash);
+        System.out.println("ExecutionManager contract hash: " + executionManagerHash);
+        System.out.println("-----------------------------------");
     }
 
     private static ContractParameter prepMsgBridgeDeployParam(BigInteger linkedChain, Hash160 managementContract,
