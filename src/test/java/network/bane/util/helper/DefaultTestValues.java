1f39d071294db7e7e3451b572592daf871cdb174
Michael Bucher <michael@axlabs.com>
2025-11-03 16:52:06 +0100

    feat: use string for version in MessageBridge and ExecutionManager

--- src/test/java/network/bane/util/helper/DefaultTestValues.java
+++ src/test/java/network/bane/util/helper/DefaultTestValues.java
@@ -10,7 +10,7 @@ public class DefaultTestValues {
 
     public static final Hash160 MANAGEMENT_CONTRACT_HASH = new Hash160("0x74eace325d73d7fa70c6b8772fabaac20a632706");
     public static final Hash160 MESSAGE_BRIDGE_CONTRACT_HASH =
-            new Hash160("0xc3bf222197606176b05b79fd38fb2bdb87042bb6");
+            new Hash160("0x217dfbf5e0cdb6226baa5d420c0655c1ba576127");
     public static final Hash160 EXECUTION_MANAGER_CONTRACT_HASH =
             new Hash160("0xd9af41a90950d2399dc2543b81368642ddd63761");
     public static final Hash160 DUMMY_EXEC_MANAGER = new Hash160("0x5cd87a79046523454325a77827006ebfae27e05f");
