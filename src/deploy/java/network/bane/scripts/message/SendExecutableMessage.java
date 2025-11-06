7f01b615d159c3860f1d7730eccafece3f221975
Michael Bucher <michael@axlabs.com>
2025-11-06 13:19:13 +0100

    chore (minor): define feeSponsor in message send scripts

--- src/deploy/java/network/bane/scripts/message/SendExecutableMessage.java
+++ src/deploy/java/network/bane/scripts/message/SendExecutableMessage.java
@@ -10,9 +10,9 @@ import io.neow3j.wallet.Account;
 import java.math.BigInteger;
 
 import static io.neow3j.transaction.AccountSigner.none;
-import static io.neow3j.types.ContractParameter.any;
 import static io.neow3j.types.ContractParameter.bool;
 import static io.neow3j.types.ContractParameter.byteArray;
+import static io.neow3j.types.ContractParameter.hash160;
 import static io.neow3j.types.ContractParameter.integer;
 import static io.neow3j.utils.Numeric.hexStringToByteArray;
 import static network.bane.scripts.message.MessageSendHelper.getMessageSendEvents;
@@ -70,7 +70,7 @@ public class SendExecutableMessage {
         Transaction tx = messageBridge.invokeFunction("sendExecutableMessage",
                         byteArray(messageData),
                         bool(storeResult),
-                        any(null),
+                        hash160(senderAcc),
                         integer(maxFee)
                 )
                 .signers(none(senderAcc).setAllowedContracts(GasToken.SCRIPT_HASH))
